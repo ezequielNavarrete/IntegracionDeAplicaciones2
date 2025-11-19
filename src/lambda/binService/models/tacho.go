1e26a81d1ffe0de03c1bdf556015a891ab7cd627
ezequielNavarrete <ezequieldario004@gmail.com>
2025-11-19 02:01:06 -0300

    se sube escuchar evento de cultura

--- src/lambda/binService/models/tacho.go
+++ src/lambda/binService/models/tacho.go
@@ -1,11 +1,16 @@
 package models
 
+import "time"
+
 type Tacho struct {
-	IDTacho   int64   `gorm:"column:id_tacho;primaryKey"`
-	IDTipo    int64   `gorm:"column:id_tipo"`
-	IDEstado  int64   `gorm:"column:id_estado"`
-	IDNeo     string  `gorm:"column:id_neo"`
-	Capacidad float64 `gorm:"column:capacidad"`
+	IDTacho          int64      `gorm:"column:id_tacho;primaryKey"`
+	IDTipo           int64      `gorm:"column:id_tipo"`
+	IDEstado         int64      `gorm:"column:id_estado"`
+	IDNeo            string     `gorm:"column:id_neo"`
+	Capacidad        float64    `gorm:"column:capacidad"`
+	Descripcion      *string    `gorm:"column:descripcion"`       // Opcional - para eventos culturales
+	Observaciones    *string    `gorm:"column:observaciones"`     // Opcional - para eventos culturales
+	FechaInstalacion *time.Time `gorm:"column:fecha_instalacion"` // Opcional - para eventos culturales
 }
 
 // TableName - nombre exacto de la tabla en MySQL
